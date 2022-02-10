341fb49e93df54ec73a7ac69993920dc793693bd
Alexandre Da Costa Monteiro <a.dcm33@gmail.com>
2022-02-10 20:07:07 +0100

    completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Nice studio in Fribourg',
+  address: 'Rue du Botzet 3 Fribourg',
+  description: 'lorem ipsum',
+  price_per_night: 40,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Spacious duplex in Zurich',
+  address: '10 EinsteinStrasse',
+  description: 'lorem ipsum',
+  price_per_night: 105,
+  number_of_guests: 4
+)
+Flat.create!(
+  name: 'Georgeous villa in Thun',
+  address: 'SeeStrasse 45',
+  description: 'Beautiful view on the lake',
+  price_per_night: 225,
+  number_of_guests: 5
+)
